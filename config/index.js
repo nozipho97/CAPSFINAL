9c3e68a68ea50f242aa567f473a292d4925aeaeb
nozipho97 <mayilanozipho@gmail.com>
2023-03-10 11:00:24 +0200

    Updates

---
+++ config/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+let { createPool } = require('mysql');
+let connection = createPool({
+    host: process.env.HOST,
+    database: process.env.dbName,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    port: process.env.dbPort,
+    multipleStatements:true
+})
+module.exports = connection;
